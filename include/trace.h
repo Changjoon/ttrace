9893bee16ba75688528047b4d861851950d7a5a5
Eunji, Lee <eunjieji.lee@samsung.com>
2016-02-25 16:40:21 +0900

    modify indentations and comments
    Change-Id: I93776ee31c68253ad287c2b95748d163530c26eb
    Signed-off-by: Eunji, Lee <eunjieji.lee@samsung.com>

--- include/trace.h
+++ include/trace.h
@@ -40,8 +40,8 @@ extern "C" {
 * @since_tizen 2.4
 */
 typedef enum {
-        TRACE_ERROR_NONE = TIZEN_ERROR_NONE,    /**< Successful */
-        TRACE_ERROR_IO_ERROR = TIZEN_ERROR_IO_ERROR     /**< I/O error */
+	TRACE_ERROR_NONE = TIZEN_ERROR_NONE,	/**< Successful */
+	TRACE_ERROR_IO_ERROR = TIZEN_ERROR_IO_ERROR	/**< I/O error */
 } trace_error_e;
 
 /**
